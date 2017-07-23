37ea736426c1991accf26127b1459952485b6a7e
Max Cherry <mxmCherry@gmail.com>
2017-07-23 12:46:19 +0300

    Cleanup && moving things around

--- internal/video/duration.go
+++ internal/video/duration.go
@@ -1,11 +1,18 @@
 package video
 
 import (
+	"bytes"
 	"fmt"
+	"os/exec"
 	"time"
 )
 
 const durationFormat = "15:04:05.999999999"
+
+var (
+	durAnchor    = []byte("Duration: ")
+	durAnchorLen = len(durAnchor)
+)
 
 var zeroTime time.Time
 
@@ -27,4 +34,25 @@ func ParseDuration(s string) (time.Duration, error) {
 
 func FormatDuration(d time.Duration) string {
 	return zeroTime.Add(d).Format(durationFormat)
+}
+
+func Duration(filename string) (time.Duration, error) {
+	out, err := exec.Command("avprobe", filename).CombinedOutput()
+	if err != nil {
+		return 0, fmt.Errorf("videothumb: failed to exec avprobe for %s: %s", filename, err.Error())
+	}
+
+	i := bytes.Index(out, durAnchor)
+	if i < 0 {
+		return 0, fmt.Errorf("videothumb: failed to parse '%s' anchor in avprobe output", durAnchor)
+	}
+	out = out[i+durAnchorLen:]
+
+	i = bytes.IndexAny(out, ",\r\n")
+	if i < 0 {
+		return 0, fmt.Errorf("videothumb: failed to parse closing comma after '%s...' in avprobe output", durAnchor)
+	}
+	out = out[0:i]
+
+	return ParseDuration(string(out))
 }
